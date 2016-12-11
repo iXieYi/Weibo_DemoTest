aed95329bba09b5e2a80eb095b2a318f0b9d218b
谢毅 <179806605@qq.com>
2016-12-11 20:10:50 +0800

    新建swift 仿写新浪微博

---
+++ Weibo_DemoTest/Weibo_DemoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weibo_DemoTest
+//
+//  Created by 谢毅 on 16/12/11.
+//  Copyright © 2016年 xieyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
